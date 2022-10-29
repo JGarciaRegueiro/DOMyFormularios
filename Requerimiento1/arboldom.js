3779ad7f4af446347850f9232642987c9ef67249
Jorge García Regueiro <jorge.garcia@bahiasoftware.es>
2022-10-29 21:38:10 +0200

    actualizando requerimiento1

--- Requerimiento1/arboldom.js
+++ Requerimiento1/arboldom.js
@@ -1,71 +1,85 @@
 //CREAR ELEMENTO FORMULARIO
+var div = document.getElementById("divformu");
+var formulario = document.createElement("form");
+div.appendChild(formulario);
+
 //INPUT DNI
-document.write("<br\>");
-document.write("DNI: ");
+var div1 = document.createElement("div");
+formulario.appendChild(div1);
 var campoDni= document.createElement("input");
+var textoDni = document.createTextNode("DNI:");
 campoDni.id = "campo1"
 campoDni.placeholder ="Ingrese su DNI";
 campoDni.setAttribute("autofocus","true");
 campoDni.setAttribute("required","true");
-divformu.appendChild(campoDni)
-document.body.appendChild(campoDni);
-document.write("<br\>");
+div1.appendChild(textoDni);
+div1.appendChild(campoDni);
+
 
 //INPUT NOMBRE
-document.write("Nombre: ");
+var div2 = document.createElement("div");
+formulario.appendChild(div2);
 var campoNombre= document.createElement("input");
+var textoNombre = document.createTextNode("Nombre:");
 campoNombre.id = "campo2"
 campoNombre.placeholder="Ingrese su nombre";
 campoNombre.setAttribute("required","true");
 campoNombre.setAttribute('maxlength',15);
-divformu.appendChild(campoNombre);
-document.body.appendChild(campoNombre);
-document.write("<br\>");
+div2.appendChild(textoNombre);
+div2.appendChild(campoNombre);
 
 //INPUT APELLIDOS
-document.write("Apellidos: ");
+var div3 = document.createElement("div");
+formulario.appendChild(div3);
 var campoApellidos= document.createElement("input");
+var textoApellidos= document.createTextNode("Apellidos:");
 campoApellidos.id = "campo3"
 campoApellidos.placeholder="Ingrese sus apellidos";
 campoApellidos.setAttribute('maxlength',30);
-document.body.appendChild(campoApellidos);
-document.write("<br\>");
+div3.appendChild(textoApellidos);
+div3.appendChild(campoApellidos);
 
 //INPUT EDAD
-document.write("Edad: ");
+var div4 = document.createElement("div");
+formulario.appendChild(div4);
 var campoEdad= document.createElement("input");
+var textoEdad= document.createTextNode("Edad:");
 campoEdad.setAttribute("type", "number");
 campoEdad.id = "campo4"
 campoEdad.setAttribute('min',18);
 campoEdad.setAttribute('max',90);
-
-document.body.appendChild(campoEdad);
-document.write("<br\>");
+div4.appendChild(textoEdad);
+div4.appendChild(campoEdad);
 
 //INPUT DIRECCION
-document.write("Direccion: ");
+var div5 = document.createElement("div");
+formulario.appendChild(div5);
 var direccion = document.createElement("input");
+var textoDireccion= document.createTextNode("Dirección:");
 direccion.id ="campo5";
 direccion.placeholder="Ingrese su dirección";
 direccion.setAttribute('maxlength',45);
-document.body.appendChild(direccion);
-document.write("<br\>");
+div5.appendChild(textoDireccion);
+div5.appendChild(direccion);
 
 //INPUT TELEFONOS
-document.write("Teléfono: ")
+var div6 = document.createElement("div");
+formulario.appendChild(div6);
 var telefonos = document.createElement("input");
+var textoTelefono= document.createTextNode("Telefono:");
 telefonos.placeholder="Formato: 123-456-7890";
 telefonos.setAttribute("type","tel");
 telefonos.setAttribute("required","true");
 telefonos.setAttribute("pattern","[0-9]{3}-[0-9]{3})-[0-9]{3}");
 telefonos.id="campo5"
-document.body.appendChild(telefonos);
-document.write("<br\>");
-document.write("<br\>");
+div6.appendChild(textoTelefono);
+div6.appendChild(telefonos);
 
 //select
-document.write("Ciudades: ");
+var div7 = document.createElement("div");
+formulario.appendChild(div7);
 var campoCiudades= document.createElement("select");
+var textoCiudades= document.createTextNode("Ciudades:");
 var opcion =document.createElement("option");
 campoCiudades.appendChild(opcion);
 var contenido=document.createTextNode("Escoja una ciudad");
@@ -77,7 +91,7 @@ var opcion1 = document.createElement("option");
 campoCiudades.appendChild(opcion1);
 var contenido1 = document.createTextNode("¡Paris!");
 opcion1.appendChild(contenido1);
-document.body.appendChild(campoCiudades);
+formulario.appendChild(campoCiudades);
 var opcion2 = document.createElement("option");
 campoCiudades.appendChild(opcion2);
 var contenido2=
@@ -87,16 +101,17 @@ var opcion3 = document.createElement("option");
 campoCiudades.appendChild(opcion3);
 var contenido3 = document.createTextNode("¡Berlin!");
 opcion3.appendChild(contenido3);
-document.body.appendChild(campoCiudades);
-document.write("<br\>");
+div7.appendChild(textoCiudades);
+div7.appendChild(campoCiudades);
 
 //bottom radio
-document.write("<br\>");
-document.write("Radio:");
-document.write("<br\>");
+var div8 = document.createElement("div");
+formulario.appendChild(div8);
+var textoRadio= document.createTextNode("Transporte:");
+formulario.appendChild(textoRadio);
 
 //Radio1
-var radio1 = document.createElement("input");   
+var radio1 = document.createElement("input");  
 radio1.type = "radio";   
 radio1.name = "radioGrp";   
 radio1.id = "rad1";   
@@ -132,8 +147,8 @@ var objLabel1 = document.createElement("label");
 objLabel1.htmlFor = radio1.id;   
 objLabel1.appendChild(radio1);   
 objLabel1.appendChild(objTextNode1)
-document.body.appendChild(objLabel1)
-document.body.appendChild(radio1)
+div8.appendChild(objLabel1)
+div8.appendChild(radio1)
 
 document.write("<br\>");
 var objTextNode2 = document.createTextNode("Barco");   
@@ -141,8 +156,8 @@ var objLabel2 = document.createElement("label");
 objLabel2.htmlFor = radio2.id;   
 objLabel2.appendChild(radio2);   
 objLabel2.appendChild(objTextNode2)
-document.body.appendChild(objLabel2)
-document.body.appendChild(radio2)
+div8.appendChild(objLabel2)
+div8.appendChild(radio2)
 
 document.write("<br\>");
 var objTextNode3 = document.createTextNode("Coche");   
@@ -150,8 +165,8 @@ var objLabel3 = document.createElement("label");
 objLabel3.htmlFor = radio3.id;   
 objLabel3.appendChild(radio3);   
 objLabel3.appendChild(objTextNode3)
-document.body.appendChild(objLabel3)
-document.body.appendChild(radio3)
+div8.appendChild(objLabel3)
+div8.appendChild(radio3)
 
 document.write("<br\>");
 var objTextNode4 = document.createTextNode("Autobus");   
@@ -159,12 +174,12 @@ var objLabel4 = document.createElement("label");
 objLabel4.htmlFor = radio4.id;   
 objLabel4.appendChild(radio4);   
 objLabel4.appendChild(objTextNode4)
-document.body.appendChild(objLabel4)
-document.body.appendChild(radio4)
+div8.appendChild(objLabel4)
+div8.appendChild(radio4)
 
 //CHECKBOX
-document.write("<br\>");
-document.write("<br\>");
+var div9 = document.createElement("div");
+formulario.appendChild(div9);
 
 var check_value = new Array( )
 check_value[0] = "I work at home"
@@ -178,9 +193,8 @@ for(var count in check_value)
     ptworkinfo.value=(check_value[count] + '</br>');
     ptworkinfo.type="checkbox";
     ptworkinfo.id="ptworkinfo" + count;
-    document.body.appendChild(ptworkinfo);
+    div9.appendChild(ptworkinfo);
     }
-document.write("<br\>");
 
 /*Lista
 var parrafo = document.createElement("p");
@@ -195,7 +209,8 @@ for ( var cosa of cosas){
 console.log(fragment);*/
 
 //AÑADIR IMAGENES 1ªforma
-document.write("<br\>");
+var div10 = document.createElement("div");
+formulario.appendChild(div10);
 //1ªIMG
 var img = new Image(150, 200);
 img.src = "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80";
@@ -203,7 +218,7 @@ img.src = "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb
 img.style.border = "2px solid black";
 img.style.borderRadius = "2px";
 img.alt="Paris"
-document.body.appendChild(img);
+div10.appendChild(img);
 //2ªIMG
 var img = new Image(200, 200);
 img.src = "https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=796&q=80";
@@ -211,7 +226,7 @@ img.src = "https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-4.
 img.style.border = "2px solid black";
 img.style.borderRadius = "2px";
 img.alt="Roma"
-document.body.appendChild(img);
+div10.appendChild(img);
 //3ªIMG
 var img = new Image(200, 200);
 img.src = "https://images.unsplash.com/photo-1587330979470-3595ac045ab0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
@@ -219,7 +234,7 @@ img.src = "https://images.unsplash.com/photo-1587330979470-3595ac045ab0?ixlib=rb
 img.style.border = "2px solid black";
 img.style.borderRadius = "2px";
 img.alt="Berlin"
-document.body.appendChild(img);
+div10.appendChild(img);
 
 //2º FORMA AÑADIR IMAGENES
 for(let i = 0; i < 3; i++) {
@@ -230,9 +245,8 @@ for(let i = 0; i < 3; i++) {
 
     }
     img.classList.add("img-margin");
-    document.body.appendChild(img);
+    div10.appendChild(img);
   }
-  document.write("<br\>");
   var input = document.createElement('TEXTAREA');
   input.placeholder="Introduzca su comentario aqui:";
   input.setAttribute('name', 'post');
@@ -240,28 +254,24 @@ for(let i = 0; i < 3; i++) {
   input.setAttribute('cols', 75);
   input.setAttribute('rows', 10);
   input.setAttribute('required','true');
-document.body.appendChild(input)
-
-document.write("<br\>");
+div10.appendChild(input)
 
 var campoEntregar= document.createElement("input");
 campoEntregar.setAttribute("type", "submit");
 campoEntregar.setAttribute("value", "Entregar");
-document.body.appendChild(campoEntregar);
+formulario.appendChild(campoEntregar);
 
 
 var campoBorrar= document.createElement("input");
 campoBorrar.setAttribute("type", "reset");
 campoBorrar.setAttribute("value", "Limpiar formulario");
-document.body.appendChild(campoBorrar);
-
-document.write("<br\>");
+formulario.appendChild(campoBorrar);
 
 var labelArchivo = document.createElement("label");
 labelArchivo.textContent="Suba el Justificante de transferencia ";
-document.body.appendChild(labelArchivo);
+formulario.appendChild(labelArchivo);
 var campoArchivo = document.createElement("input");
 campoArchivo.setAttribute("type", "file");
 campoArchivo.setAttribute("name", "img");
 //campoArchivo.setAttribute(multiple);
-document.body.appendChild(campoArchivo);
+formulario.appendChild(campoArchivo);
