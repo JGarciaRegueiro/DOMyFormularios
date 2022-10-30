82f7e9061d82a8f457297e793322febb71755198
Jorge García Regueiro <jorge.garcia@bahiasoftware.es>
2022-10-30 20:09:14 +0100

    actualizando requerimiento2

--- Requerimiento2/pizza.js
+++ Requerimiento2/pizza.js
@@ -2,12 +2,32 @@ var nombre = document.getElementById("nombre");
 var direccion = document.getElementById("direccion");
 var telefono = document.getElementById("telefono");
 var email = document.getElementById("email");
+var tamaño = document.getElementsByName("tamaño");
+    var ingrediente = document.getElementsByName("ingrediente");
+var checked = false;
+var precio = 0;
 
 function calcularPrecio (){
-    var precioPequeña = 5;
-    var precioMediana = 5;
-    var precioGrande = 5;
-    alert("El precio total del pedido es $precio")
+    for (k=0; k<tamaño.length;k++){
+        if (tamaño[k].checked){
+            var precioPorTamaño = tamaño[k].value;
+        }
+    }
+    if (precioPorTamaño.toString() == 'pequeña'){
+        precio=precio+5;
+    }
+    if (precioPorTamaño.toString() == 'mediana'){
+        precio=precio+10;
+    }
+    if (precioPorTamaño.toString() == 'grande'){
+        precio=precio+15;
+    }
+    for (h=0; h<ingrediente.length;h++){
+        if (ingrediente[h].checked){
+            precio=precio+1;
+        }
+    }
+    alert("El precio total del pedido es " + precio)
 }
 
 function validar (){
@@ -25,5 +45,26 @@ function validar (){
 
     if (email.value.trim() == ''){
         alert("El email es obligatorio")
+    }
+
+    for (i=0; i<tamaño.length;i++){
+        if (tamaño[i].checked){
+            checked = true;
+            break;
+        }
+    }
+    if (!checked){
+        alert("El tamaño es obligatorio")
+    }
+
+    checked = false;
+    for (j=0; j<ingrediente.length;j++){
+        if (ingrediente[j].checked){
+            checked = true;
+            break;
+        }
+    }
+    if (!checked){
+        alert("El ingrediente es obligatorio")
     }
 }
