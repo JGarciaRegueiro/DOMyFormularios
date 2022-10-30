369cfe635c3c94ac4d716ff764941c3521153596
Jorge García Regueiro <jorge.garcia@bahiasoftware.es>
2022-10-30 20:35:04 +0100

    actualizando requerimiento2

--- Requerimiento2/pizza.js
+++ Requerimiento2/pizza.js
@@ -3,48 +3,29 @@ var direccion = document.getElementById("direccion");
 var telefono = document.getElementById("telefono");
 var email = document.getElementById("email");
 var tamaño = document.getElementsByName("tamaño");
-    var ingrediente = document.getElementsByName("ingrediente");
+var ingrediente = document.getElementsByName("ingrediente");
 var checked = false;
 var precio = 0;
-
-function calcularPrecio (){
-    for (k=0; k<tamaño.length;k++){
-        if (tamaño[k].checked){
-            var precioPorTamaño = tamaño[k].value;
-        }
-    }
-    if (precioPorTamaño.toString() == 'pequeña'){
-        precio=precio+5;
-    }
-    if (precioPorTamaño.toString() == 'mediana'){
-        precio=precio+10;
-    }
-    if (precioPorTamaño.toString() == 'grande'){
-        precio=precio+15;
-    }
-    for (h=0; h<ingrediente.length;h++){
-        if (ingrediente[h].checked){
-            precio=precio+1;
-        }
-    }
-    alert("El precio total del pedido es " + precio)
-}
 
 function validar (){
     if (nombre.value.trim() == ''){
         alert("El nombre es obligatorio")
+        return false;
     }
 
     if (direccion.value.trim() == ''){
         alert("La direccion es obligatoria")
+        return false;
     }
 
     if (telefono.value.trim() == ''){
         alert("El teléfono es obligatorio")
+        return false;
     }
 
     if (email.value.trim() == ''){
         alert("El email es obligatorio")
+        return false;
     }
 
     for (i=0; i<tamaño.length;i++){
@@ -55,6 +36,7 @@ function validar (){
     }
     if (!checked){
         alert("El tamaño es obligatorio")
+        return false;
     }
 
     checked = false;
@@ -66,5 +48,35 @@ function validar (){
     }
     if (!checked){
         alert("El ingrediente es obligatorio")
+        return false;
     }
+
+    else return true;
+
+}
+
+function calcularPrecio (){
+    if (validar () == true){
+        for (k=0; k<tamaño.length;k++){
+            if (tamaño[k].checked){
+                var precioPorTamaño = tamaño[k].value;
+            }
+        }
+        if (precioPorTamaño == 'pequeña'){
+            precio=precio+5;
+        }
+        if (precioPorTamaño == 'mediana'){
+            precio=precio+10;
+        }
+        if (precioPorTamaño == 'grande'){
+            precio=precio+15;
+        }
+        for (h=0; h<ingrediente.length;h++){
+            if (ingrediente[h].checked){
+                precio=precio+1;
+            }
+        }
+        alert("El precio total del pedido es " + precio +"€") 
+    }
+    
 }
