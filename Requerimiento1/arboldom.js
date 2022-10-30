7d2b181fd6de57350f24e4869e00a65f929f62ee
NOMBRE QUE USAREMOS PARA MOSTRAR EN GIT <mam3192@gmail.com>
2022-10-30 17:20:40 +0100

    modificando comentarios

--- Requerimiento1/arboldom.js
+++ Requerimiento1/arboldom.js
@@ -40,15 +40,15 @@ div1.appendChild(campoDni);
 
 
 //INPUT NOMBRE
-//Crer nodo de tipo Element div2
+//Crear nodo de tipo Element div2
 var div2 = document.createElement("div");
 
 //Añadir el nodo Element como hijo de la pagina
 formulario.appendChild(div2);
 
-//Crer nodo de tipo Element
+//Crear nodo de tipo Element
 var campoNombre= document.createElement("input");
-//Crer nodo de tipo Text
+//Crear nodo de tipo Text
 var textoNombre = document.createTextNode("Nombre:");
 
 campoNombre.id = "campo2"//<input id= "campoNombre"></input>
@@ -62,7 +62,7 @@ div2.appendChild(textoNombre);
 div2.appendChild(campoNombre);
 
 //INPUT APELLIDOS
-//Crer nodo de tipo Element div3
+//Crear nodo de tipo Element div3
 var div3 = document.createElement("div");
 
 //Añadir el nodo Element como hijo de la pagina
@@ -95,7 +95,7 @@ div4.appendChild(textoEdad);
 div4.appendChild(campoEdad);
 
 //INPUT DIRECCION
-//Crer nodo de tipo Element div5
+//Crear nodo de tipo Element div5
 var div5 = document.createElement("div");
 formulario.appendChild(div5);
 var direccion = document.createElement("input");
@@ -107,7 +107,7 @@ div5.appendChild(textoDireccion);
 div5.appendChild(direccion);
 
 //INPUT TELEFONOS
-//Crer nodo de tipo Element div6
+//Crear nodo de tipo Element div6
 var div6 = document.createElement("div");
 formulario.appendChild(div6);
 var telefonos = document.createElement("input");
@@ -121,7 +121,7 @@ div6.appendChild(textoTelefono);
 div6.appendChild(telefonos);
 
 //select
-//Crer nodo de tipo Element div7
+//Crear nodo de tipo Element div7
 var div7 = document.createElement("div");
 formulario.appendChild(div7);
 var campoCiudades= document.createElement("select");
@@ -132,38 +132,38 @@ var contenido=document.createTextNode("Escoja una ciudad");
 opcion.setAttribute("selected","enable");
 opcion.appendChild(contenido);
 
-//Crer nodo de tipo Element opcion1
+//Crear nodo de tipo Element opcion1
 var opcion1 = document.createElement("option");
 //campoCiudades.id = "ciudades"
 campoCiudades.appendChild(opcion1);
-//Crer nodo de tipo Text contenido1
+//Crear nodo de tipo Text contenido1
 var contenido1 = document.createTextNode("¡Paris!");
 opcion1.appendChild(contenido1);
-//Crer nodo de tipo Element opcion2
+//Crear nodo de tipo Element opcion2
 var opcion2 = document.createElement("option");
 campoCiudades.appendChild(opcion2);
 //Crer nodo de tipo Text contenido2
 var contenido2=document.createTextNode("¡Roma!");
 opcion2.appendChild(contenido2);
-//Crer nodo de tipo Element opcion3
+//Crear nodo de tipo Element opcion3
 var opcion3 = document.createElement("option");
 campoCiudades.appendChild(opcion3);
-//Crer nodo de tipo Text contenido3
+//Crear nodo de tipo Text contenido3
 var contenido3 = document.createTextNode("¡Berlin!");
 opcion3.appendChild(contenido3);
 div7.appendChild(textoCiudades);
 div7.appendChild(campoCiudades);
 
 //bottom radio
-//Crer nodo de tipo Element div8
+//Crear nodo de tipo Element div8
 var div8 = document.createElement("div");
 formulario.appendChild(div8);
-//Crer nodo de tipo Text textoRadio
+//Crear nodo de tipo Text textoRadio
 var textoRadio= document.createTextNode("Transporte:");
 div8.appendChild(textoRadio);
 
 //Radio1
-//Crer nodo de tipo Element radio1
+//Crear nodo de tipo Element radio1
 var radio1 = document.createElement("input");  
 radio1.type = "radio";   
 radio1.name = "radioGrp";   
@@ -173,7 +173,7 @@ radio1.defaultChecked = true;
 radio1.checked = true; 
 
 //Radio2
-//Crer nodo de tipo Element radio2
+//Crear nodo de tipo Element radio2
 var radio2 = document.createElement("input");   
 radio2.type = "radio";   
 radio2.name = "radioGrp";   
@@ -181,23 +181,23 @@ radio2.id = "rad2";
 radio2.value = "miradio2"; 
    
 //Radio3
-//Crer nodo de tipo Element radio3
+//Crear nodo de tipo Element radio3
 var radio3 = document.createElement("input");   
 radio3.type = "radio";   
 radio3.name = "radioGrp";   
 radio3.id = "rad3";   
 radio3.value = "miradio3"; 
 //Radio4
-//Crer nodo de tipo Element radio4
+//Crear nodo de tipo Element radio4
 var radio4 = document.createElement("input");   
 radio4.type = "radio";   
 radio4.name = "radioGrp";   
 radio4.id = "rad4";   
 radio4.value = "miradio4"; 
 
-//Crer nodo de tipo Text objTextNode1
+//Crear nodo de tipo Text objTextNode1
 var objTextNode1 = document.createTextNode("Avión"); 
-//Crer nodo de tipo Element objLabel1
+//Crear nodo de tipo Element objLabel1
 var objLabel1 = document.createElement("label");   
 objLabel1.htmlFor = radio1.id;   
 objLabel1.appendChild(radio1);   
@@ -205,9 +205,9 @@ objLabel1.appendChild(objTextNode1)
 div8.appendChild(objLabel1)
 div8.appendChild(radio1)
 
-//Crer nodo de tipo Text objTextNode2
+//Crear nodo de tipo Text objTextNode2
 var objTextNode2 = document.createTextNode("Barco");  
-//Crer nodo de tipo Element objLabel2
+//Crear nodo de tipo Element objLabel2
 var objLabel2 = document.createElement("label");   
 objLabel2.htmlFor = radio2.id;   
 objLabel2.appendChild(radio2);   
@@ -215,9 +215,9 @@ objLabel2.appendChild(objTextNode2)
 div8.appendChild(objLabel2)
 div8.appendChild(radio2)
 
-//Crer nodo de tipo Text objTextNode3
+//Crear nodo de tipo Text objTextNode3
 var objTextNode3 = document.createTextNode("Coche");   
-//Crer nodo de tipo Element objLabel3
+//Crear nodo de tipo Element objLabel3
 var objLabel3 = document.createElement("label");   
 objLabel3.htmlFor = radio3.id;   
 objLabel3.appendChild(radio3);   
@@ -225,9 +225,9 @@ objLabel3.appendChild(objTextNode3)
 div8.appendChild(objLabel3)
 div8.appendChild(radio3)
 
-//Crer nodo de tipo Text objTextNode4
+//Crear nodo de tipo Text objTextNode4
 var objTextNode4 = document.createTextNode("Autobus"); 
-//Crer nodo de tipo Element objLabel4
+//Crear nodo de tipo Element objLabel4
 var objLabel4 = document.createElement("label");   
 objLabel4.htmlFor = radio4.id;   
 objLabel4.appendChild(radio4);   
@@ -236,7 +236,7 @@ div8.appendChild(objLabel4)
 div8.appendChild(radio4)
 
 //CHECKBOX
-//Crer nodo de tipo Element div9
+//Crear nodo de tipo Element div9
 var div9 = document.createElement("div");
 formulario.appendChild(div9);
 
